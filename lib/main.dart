6dcdaf492a4cdc0e26c66c7dac6c8c709af30999
rashedi28 <ahmedalrashedi333@gmail.com>
2024-10-09 23:54:36 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BookStore());
+}
+
+class BookStore extends StatelessWidget {
+  const BookStore({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
